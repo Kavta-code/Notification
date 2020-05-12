fdb0a45754f93f8d89a92a227bc4ec361addba55
Kavta-code <kavitha.y05@gmail.com>
2020-05-12 11:35:40 +0530

    Notification Code

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.notification;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "personal notification";
+    private final int NOTIFICATION_ID =001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayNotification(View view) {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_sms_notification);
+        builder.setContentTitle("Simple Notification");
+        builder.setContentText("This is an example for simple notification");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID,builder.build());
+    }
+}
